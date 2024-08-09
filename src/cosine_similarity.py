dd1de52ab86fbcfe1bc38a2060bc43e357259e9d
Aidan Goeschel <aidangoeschel@gmail.com>
2024-08-08 23:12:17 -0700

    added skeleton for cosine similarity using google universla sentence encoder

---
+++ src/cosine_similarity.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def vectorize(string : str) -> np.array:
+    pass
+
+
+def compute_cosine_similarity(string1 : str, string2 : str) -> float:
+    pass
+
+
+if __name__ == "__main__":
+    pass
