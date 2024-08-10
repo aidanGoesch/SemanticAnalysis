4418505db883449d8c97cb2bd5b9614adada09c7
Aidan Goeschel <aidangoeschel@gmail.com>
2024-08-09 22:55:46 -0700

    implementation of cosine similiarity between texts using Google Universal Sentence Encoder

--- src/cosine_similarity.py
+++ src/cosine_similarity.py
@@ -1,13 +1,25 @@
+
+import tensorflow_hub as hub
 import numpy as np
+from numpy.linalg import norm
 
 
-def vectorize(string : str) -> np.array:
-    pass
+def compute_cosine_similarity(string1 : str, string2 : str, verbose : bool = False) -> float:
+    embed = hub.load(
+        "https://www.kaggle.com/models/google/universal-sentence-encoder/TensorFlow2/universal-sentence-encoder/2")
+    embeddings = embed([string1, string2])
 
+    if verbose:
+        print(embeddings)
 
-def compute_cosine_similarity(string1 : str, string2 : str) -> float:
-    pass
+    # compute the cosine similarity
+    similarity = np.dot(embeddings[0], embeddings[1]) / (norm(embeddings[0]) * norm(embeddings[1]))
+
+    return similarity
 
 
 if __name__ == "__main__":
-    pass
+    description1 = "I remember the first time that I had alcohol very well. It was me and my friends at a bar and we laughed and had a jolly time!"
+    description2 = "The most memorable event I had involving alcohol has to be that time when I was in a bar in mexico. I was alone and drank until I couldn't find my feet"
+
+    print(compute_cosine_similarity(description1, description2, True))
