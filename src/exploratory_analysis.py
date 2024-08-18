32c69ca6c933cc1d4c6bf505e02b81171ee91e8c
Aidan Goeschel <aidangoeschel@gmail.com>
2024-08-18 16:57:27 -0700

    prototype for POS tagging - pending specific POS

--- src/exploratory_analysis.py
+++ src/exploratory_analysis.py
@@ -1,6 +1,9 @@
 import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 class ExploratoryAnalysis:
     def __init__(self, text : str):
@@ -38,9 +41,36 @@ class ExploratoryAnalysis:
         """calculates the number of words and sentences in the text"""
         pass
 
-    def part_of_speech_tagging(self):
+    def part_of_speech_tagging(self, verbose : bool = False):
         """identifies nouns, verbs, and adjectives in the text"""
-        pass
+        tagged = []
+        # sent_tokenize is one of instances of PunktSentenceTokenizer from the nltk.tokenize.punkt module
+        tokenized = sent_tokenize(self.text)
+        for i in tokenized:
+            # Word tokenizers is used to find the words and punctuation in a string
+            wordsList = nltk.word_tokenize(i)
+
+            # removing stop words from wordList
+            wordsList = [w for w in wordsList if not w in ENGLISH_STOP_WORDS]
+
+            tagged = nltk.pos_tag(wordsList)
+            if verbose:
+                print(tagged)
+
+        # Go through and count the occurences of each POS
+        res = {}
+        for word, pos in tagged:
+            if pos in res:
+                res[pos] += 1
+            else:
+                res[pos] = 1
+
+        if verbose:
+            for key, value in res.items():
+                print(f"{key} : {value}")
+
+        self.analysis_results['part of speech tagging'] = res
+
 
     def first_person_statements(self):
         """identifies and counts the number of 'I' statements in the text"""
@@ -70,4 +100,4 @@ if __name__ == "__main__":
     model = ExploratoryAnalysis(
         "In parallel, and in keeping with previous fndings in similarrepeated decision-making tasks, chronological age was associated with increasing noisiness of choices relative tovalues estimated using standard reinforcement learning, and a concurrent increase in perseverative responding. In Experiment 2, we delved further into the relationship between memory precision and choice, by identifying a role for memory precision in selecting which memories are sampled. Specifcally, we designed a variant of the previous task in which sampled context memories could be identifed as specifc or ‘gist’-level (e.g. ‘beaches’ as opposed to ‘that one particular beach’), with each having distinct, opposing efects on choice. We found that lower memory precision was associated with a greater reliance on gist-based memory during memory sampling"
     )
-    model.bag_of_words(True)
+    model.part_of_speech_tagging(True)
