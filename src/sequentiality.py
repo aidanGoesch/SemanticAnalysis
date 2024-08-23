5bded079a7e760e2c06f9b64ecc84fe3577c7c89
Aidan Goeschel <aidangoeschel@gmail.com>
2024-08-22 22:37:01 -0700

    new contextually dependent sequentiality function

--- src/sequentiality.py
+++ src/sequentiality.py
@@ -27,12 +27,17 @@ class SequentialityModel:
         self.sentence = ""  # this is what sequentiality is calculated on
         self.stem = ""  # this is a slice of the sentence - used for context for model
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=HUGGING_FACE_TOKEN, use_safetensors = True)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name,
+                                                       token=HUGGING_FACE_TOKEN,
+                                                       use_safetensors=True,
+                                                       padding_side="left")
         self.model = AutoModelForCausalLM.from_pretrained(model_name,
                                                           token=HUGGING_FACE_TOKEN,
                                                           torch_dtype=torch.bfloat16,
                                                           device_map=mps_device,
                                                           use_safetensors=True).to(mps_device)
+
+        self.model.config.pad_token_id = self.model.config.eos_token_id
         # turn off gradient descent
         torch.set_grad_enabled(False)
 
@@ -42,7 +47,8 @@ class SequentialityModel:
         self.memoize = {}
 
     def _to_tokens_and_logprobs(self, input_texts):
-        input_ids = self.tokenizer(input_texts, padding=True, return_tensors="pt").input_ids
+        # manually pad every input string with an underscore
+        input_ids = self.tokenizer("_" + input_texts, padding=True, return_tensors="pt").input_ids.to(mps_device)
         outputs = self.model(input_ids)
         probs = torch.log_softmax(outputs.logits, dim=-1).detach()
 
@@ -62,30 +68,57 @@ class SequentialityModel:
 
 
 
-    def calculate_sequentiality(self, sentence : str, i : int,  h : int, verbose : bool = False) -> float:
-        """Returns the sum of the likelihoods of each word in a sentence. This may need to change
-        depending on how the transcription works and seperates sentences."""
-
+    def calculate_contextual_sequentiality(self, sentence : str, i : int,  h : int, verbose : bool = False) -> float:
+        """Calculate the contextually dependent sequentiality of a sentence."""
+        raw_sequentiality = 0
         if i - h < 0:
             context = ". ".join(self.sentences[:i])
 
         else:
             context = ". ".join(self.sentences[i - h:i])
 
-        print(context)
+        if len(context) == 0:  # beginning of the text - prevents random period at the front of the text
+            input_text = sentence + "."
+        else:
+            input_text = context + ". " + sentence + "."
 
+        tokens_and_logprobs = self._to_tokens_and_logprobs(input_text)[0]
 
+        tokenized_sentence = self.tokenizer.tokenize(sentence)
+
+        first_token = tokenized_sentence[0].strip("▁")
+
+        for j, tmp in enumerate(reversed(tokens_and_logprobs)):
+            if tmp[0].lower() == first_token.lower():
+                raw_sequentiality = sum(map(lambda x: x[1], tokens_and_logprobs[j:]))
+                break
+
+        return raw_sequentiality
+
+    def calculate_topic_sequentiality(self, sentence : str, verbose : bool = False) -> float:
+        """Calculate the sequentiality of a sentence given only a topic"""
+        pass
+
+    def calculate_sequentiality(self, sentence : str, i: int, verbose : bool = False) -> float:
+        """Calculates the sequentiality of a given sentence by subtracting the context dependent sequentiality from
+        the purely topic driven version."""
+        tokenized_sentence = self.tokenizer.tokenize(sentence)
+
+        topic_sequentiality = self.calculate_topic_sequentiality(sentence)
+        contextual_sequentiality = self.calculate_contextual_sequentiality(sentence, i, CALL_BACK, False)
+
+        return -(topic_sequentiality - contextual_sequentiality) / len(tokenized_sentence)
 
     def calculate_total_sequentialty(self, text : str, verbose : bool = False) -> float:
         self.sentences = re.split('[\.\?\!]\s*', text)
         sequentialities = []
 
         for i, sentence in enumerate(self.sentences):
+            if sentence == "": break
+
             sequentialities.append(self.calculate_sequentiality(sentence, i, CALL_BACK, False))
 
         return np.mean(sequentialities)
-
-
 
 
 if __name__ == "__main__":
@@ -94,10 +127,10 @@ if __name__ == "__main__":
     # print(f"\ntotal likelihood = {model.calculate_sequentiality("It is nice to meet you.", False)}")
     import time
 
-    model.sentences = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-
-    model.calculate_sequentiality("test", 1, CALL_BACK, False)
+    # model.sentences = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    #
+    # model.calculate_sequentiality("test", 1, CALL_BACK, False)
 
     # start = time.time()
-    # print(f"\ntotal NLL of test scene= {model.calculate_sequentiality('"THE BOYFRIEND" in bold white text fades in on a black screen before fading out. The letters of "high maintenance" appear in the center of the screen one by one in white text. A simple jingle plays in the background. ', False)}")
+    print(f"\ntotal NLL of test scene= {model.calculate_total_sequentialty('"THE BOYFRIEND" in bold white text fades in on a black screen before fading out. The letters of "high maintenance" appear in the center of the screen one by one in white text. A simple jingle plays in the background.', False)}")
     # print("time to run: ", time.time() - start)
