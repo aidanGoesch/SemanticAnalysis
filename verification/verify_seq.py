8a362d9523ffdd02ec7f4405af538eab0657e093
Aidan Goeschel <aidangoeschel@gmail.com>
2024-10-09 10:04:46 -0700

    Fleshed out code for verification

--- verification/verify_seq.py
+++ verification/verify_seq.py
@@ -1,30 +1,34 @@
 import sys
 import numpy as np
+import pandas as pd
 from src.sequentiality import SequentialityModel
 
 
 def load_data():
-    return [[]]
+    df = pd.read_csv("./data/hcV3-stories.csv")
+    return df
 
 
-def write_data(file_name):
-    pass
+def write_data(file_name, data : pd.DataFrame):
+    data.to_csv(f"./data/calculated_values/{file_name}")
 
 
 def verify_data(start : int, stop : int):
-    sequentialities = []
+    sequentialities = pd.DataFrame(columns=["sequentiality"])
     data = load_data()
 
     data_slice = data[start:stop]
 
-    model = SequentialityModel("microsoft/Phi-3-mini-4k-instruct", topic="a conversation with a doctor")
+    # model = SequentialityModel("microsoft/Phi-3-mini-4k-instruct", topic="a conversation with a doctor")
 
-    for person in data_slice:
-        seq = model.calculate_total_sequentiality(person.story)
-        sequentialities.append(seq)
+    for i in range(len(data_slice)):
+        # seq = model.calculate_total_sequentiality(person.story)
+        # sequentialities.append(seq)
+        sequentialities.loc[i] = "this is a test"
 
-    write_data(f"{start}-{stop}.csv")
+    write_data(f"{start}-{stop}.csv", sequentialities)
 
 
 if __name__ == "__main__":
-    verify_data(*sys.argv)
+    # verify_data(*sys.argv)
+    load_data()
