26754f9d8944e92f8cb0637e98382f2ab9215482
Aidan Goeschel <aidangoeschel@gmail.com>
2024-10-09 17:45:11 -0700

    minor bug fix

--- verification/verify_seq.py
+++ verification/verify_seq.py
@@ -14,7 +14,8 @@ def write_data(file_name, data : pd.DataFrame):
 
 
 def verify_data(partition_id, participant_id):
-    sequentialities = pd.DataFrame(columns=["scalar_text_sequentiality",
+    sequentialities = pd.DataFrame(columns=["AssignmentId",
+                                            "scalar_text_sequentiality",
                                             "sentence_total_sequentialities",
                                             "sentence_contextual_sequentialities",
                                             "sentence_topic_sequentialities",
@@ -28,7 +29,7 @@ def verify_data(partition_id, participant_id):
     model = SequentialityModel("microsoft/Phi-3-mini-4k-instruct", topic="a conversation with a doctor")
 
     seq = model.calculate_text_sequentiality(vec.story)
-    sequentialities.loc[0] = seq + [vec.story, vec.recAgnPairId, vec.recImgPairId]
+    sequentialities.loc[0] = [vec.AssignmentId] + seq + [vec.story, vec.recAgnPairId, vec.recImgPairId]
 
     write_data(f"{partition_id + participant_id}.csv", sequentialities)
 
