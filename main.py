dee624ee0dafb1032d80160ae2d15d7a38395654
Aidan Goeschel <aidangoeschel@gmail.com>
2024-12-16 21:50:34 -0800

    cleaned up code and added comments

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from src.sequentiality import SequentialityModel
-from RPE.reader import read_csv
-import numpy as np
 from verification.verify_seq import *
-from verification.processing import merge_csvs
 from verification.generate_plots import generate
 import pandas as pd
 
 
-# use slice notation for scenes
 
-# TODO: sliding window - only use the last 4 sentences
 
-def calculate_cumulative_sequentiality(data: np.array, start: int = None,
-                                       stop: int = None):  # TODO: Add this to the SequentialityModel class
+def calculate_cumulative_sequentiality(data: np.array, start: int = None, stop: int = None):
+    """!!! NOT USED ON HPC3 !!!"""
     model = SequentialityModel("microsoft/Phi-3-mini-4k-instruct", topic="a description of a film")
 
     sequentialities = []
@@ -24,7 +18,7 @@ def calculate_cumulative_sequentiality(data: np.array, start: int = None,
         print("-------------------------------------------------------------------------------------------")
         print("text:" + " ".join(data[:i]))
         v = i >= 100
-        seq = model.calculate_total_sequentiality(" ".join(data[:i + 1]),
+        seq = model.calculate_text_sequentiality(" ".join(data[:i + 1]),
                                                   v)  # calculate the sequentiality up until every scene
         sequentialities.append(seq)  # can sum or do something with them
 
@@ -34,8 +28,8 @@ def calculate_cumulative_sequentiality(data: np.array, start: int = None,
     print(f"sequentialities = {sequentialities}")
 
 
-def calculate_individual_sequentiality(data: np.array, start: int = None,
-                                       stop: int = None):  # TODO: Add this to the SequentialityModel class
+def calculate_individual_sequentiality(data: np.array, start: int = None, stop: int = None):
+    """!!! NOT USED ON HPC3 !!!"""
     model = SequentialityModel("microsoft/Phi-3-mini-4k-instruct", topic="a description of a film")
 
     sequentialities = []
@@ -47,7 +41,7 @@ def calculate_individual_sequentiality(data: np.array, start: int = None,
         print("-------------------------------------------------------------------------------------------")
         print("text:" + data[i])
         v = i >= 100
-        seq = model.calculate_total_sequentiality(data[i], v)  # calculate the sequentiality up until every scene
+        seq = model.calculate_text_sequentiality(data[i], v)  # calculate the sequentiality up until every scene
         sequentialities.append(seq)  # can sum or do something with them
 
         print(f"Sequentiality of scene {i + 1}: {seq}")
@@ -56,15 +50,17 @@ def calculate_individual_sequentiality(data: np.array, start: int = None,
     print(f"sequentialities = {sequentialities}")
 
 def main():
-    df = pd.read_csv("./data/main.csv")
-    generate(df)
+    """function that generates the graph from the calculated sequentiality values"""
+
+    dfs = []
+    for i in range(9):
+        dfs.append(pd.read_csv(f"./data/calculated_values/{i + 1}/main.csv"))
+
+    generate(dfs)
 
 
 if __name__ == '__main__':
-    # d = read_csv("RPE/preliminary_analysis/scene descriptions.csv")
-    # calculate_cumulative_sequentiality(d)
-    # calculate_individual_sequentiality(d)
+    # this is how it was run on hpc3 - function is in verification/verify_seq.py
+    # verify_data(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 
-    verify_data(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
-
-    # main()
+    main()
--- verification/generate_plots.py
+++ verification/generate_plots.py
@@ -4,41 +4,43 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-def generate(df : pd.DataFrame):
-    # normalize the sequentiality values
-    scaler = MinMaxScaler()
-    df["scalar_text_sequentiality"] = pd.DataFrame(scaler.fit_transform(df[["scalar_text_sequentiality"]]),
-                                                   columns=["sequentiality"])
-
-    df['seq_type'] = df.apply(determine_bin, axis=1)
-
-    # Calculate average sequentiality for each bin type
-    df['seq_type'] = df.apply(determine_bin, axis=1)
-
-    # Calculate average sequentiality for each bin type
-    bin_averages = df.groupby('seq_type')['scalar_text_sequentiality'].mean()
-
-    imagined_recalled_diff = percentage_dif(bin_averages['Imagined'], bin_averages['Recall'])
-    imagined_retold_diff = percentage_dif(bin_averages['Imagined'], bin_averages['Retold'])
-    retold_recalled_diff = percentage_dif(bin_averages['Retold'], bin_averages['Recall'])
-
-    # Create tuple of the three bin averages
-    pts = [(imagined_recalled_diff, imagined_retold_diff, retold_recalled_diff)]
-
-    x = [1]
+def generate(dfs : [pd.DataFrame]):
+    x = []
 
     imagined_recalled = []
     imagined_retold = []
     retold_recalled = []
 
-    for i, j, k in pts:
-        imagined_recalled.append(i)
-        imagined_retold.append(j)
-        retold_recalled.append(k)
+    for idx, df in enumerate(dfs):
+        # normalize the sequentiality values
+        scaler = MinMaxScaler()
+        df["scalar_text_sequentiality"] = pd.DataFrame(scaler.fit_transform(df[["scalar_text_sequentiality"]]),
+                                                       columns=["sequentiality"])
+
+        df['seq_type'] = df.apply(determine_bin, axis=1)
+
+        # Calculate average sequentiality for each bin type
+        df['seq_type'] = df.apply(determine_bin, axis=1)
+
+        # Calculate average sequentiality for each bin type
+        bin_averages = df.groupby('seq_type')['scalar_text_sequentiality'].mean()
+
+        imagined_recalled_diff = percentage_dif(bin_averages['Imagined'], bin_averages['Recall'])
+        imagined_retold_diff = percentage_dif(bin_averages['Imagined'], bin_averages['Retold'])
+        retold_recalled_diff = percentage_dif(bin_averages['Retold'], bin_averages['Recall'])
+
+        # Create tuple of the three bin averages
+        pts = [(imagined_recalled_diff, imagined_retold_diff, retold_recalled_diff)]
+
+        x.append(idx)
+        for i, j, k in pts:
+            imagined_recalled.append(i)
+            imagined_retold.append(j)
+            retold_recalled.append(k)
 
     plt.figure()
     plt.title("Prelim recreation")
-    plt.xlabel("normalized sequentiality")
+    plt.xlabel("recall")
     plt.ylabel("% difference")
 
     plt.scatter(x, imagined_recalled, label="imagined vs. recalled", color="blue")
@@ -74,69 +76,6 @@ def percentage_dif(l, r):
         denominator = 0.0000001
 
     return (numerator / denominator) * 100
-
-def get_pts(df : pd.DataFrame, verbose : bool = False):
-    """This is wrong and where the problem is"""
-    thresholds = [0, 0.111111, 0.222222, 0.333333, 0.444444, 0.555555, 0.666666, 0.777777, 0.888888, 1]
-    labels = ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9"]
-
-    bins = df['main_bins'] = pd.cut(df['scalar_text_sequentiality'], bins=thresholds, labels=labels,
-                                    include_lowest=True)
-
-    if verbose:
-        bin_counts = bins.value_counts()
-        print("Total number of values in each bin:")
-        print(bin_counts)
-
-    # recAgnPairId = None for imagined stories -> ID of the retold story being recalled
-    # if not None then its recalled
-
-    # recImgPairId = None for retold stories -> ID of imagined story being recalled
-    # if not None then its recalled
-
-    df['sub_bins'] = df.apply(determine_bin, axis=1)
-
-    if verbose:
-        sub_bin_counts = df['sub_bins'].value_counts()
-        print("\nCount of values in each sub-bin:")
-        print(sub_bin_counts)
-
-    grouped_means = df.groupby(['main_bins', 'sub_bins'])["scalar_text_sequentiality"].mean()
-    grouped_means = grouped_means.fillna(0)
-
-    percentage_differences = {}
-
-    for main_bin in labels:
-        if main_bin in grouped_means.index.get_level_values(0):
-            # Get the means for the current main bin
-            means = grouped_means.loc[main_bin]
-
-            # Extract the values for each sub-bin
-            both_none_mean = grouped_means.get((main_bin, 'Both None'), 0)
-            a_present_b_none_mean = grouped_means.get((main_bin, 'recAgnPairId Not None'), 0)
-            b_present_a_none_mean = grouped_means.get((main_bin, 'recImgPairId Not None'), 0)
-
-            if verbose:
-                print(f"values: {both_none_mean}, {a_present_b_none_mean}, {b_present_a_none_mean}")
-                print(means)
-
-            # Calculate the percentage differences
-            diff1 = percentage_dif(a_present_b_none_mean, both_none_mean)
-            diff2 = percentage_dif(b_present_a_none_mean, a_present_b_none_mean)
-            diff3 = percentage_dif(both_none_mean, b_present_a_none_mean)
-
-            # Store the tuple in the dictionary
-            percentage_differences[main_bin] = (diff1, diff2, diff3)
-
-    if verbose:
-        # Print the percentage differences for each main bin
-        print("\nPercentage differences for each main bin:")
-        for bin_label, diffs in percentage_differences.items():
-            print(f"{bin_label}: {diffs}")
-
-    return percentage_differences
-
-
 
 
 if __name__ == "__main__":
