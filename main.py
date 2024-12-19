fb6a2e7e217f3a3ff3634892b13cefb4c74671e7
Aidan Goeschel <aidangoeschel@gmail.com>
2024-12-19 13:01:10 -0800

    added code to test new LLMs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from verification.verify_seq import *
-from verification.generate_plots import generate
+from verification.generate_plots import generate_2d, generate_2a, create_balanced_dataset
 import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import gc
 
 
 
@@ -56,11 +59,35 @@ def main():
     for i in range(9):
         dfs.append(pd.read_csv(f"./data/calculated_values/{i + 1}/main.csv"))
 
-    generate(dfs)
+    generate_2a(dfs)
+
+def explore_random_seeds():
+    s = []
+    for i in range(30):
+        torch.manual_seed(i)  # Add before model initialization
+        np.random.seed(i)
+        print(f"============{i} started============")
+        model = SequentialityModel("microsoft/Phi-3-mini-4k-instruct", topic="a short story")
+        s.append(model.calculate_text_sequentiality(
+            "Concerts are my most favorite thing, and my boyfriend knew it. That's why, for our anniversary, he got me tickets to see my favorite artist. Not only that, but the tickets were for an outdoor show, which I love much more than being in a crowded stadium. Since he knew I was such a big fan of music, he got tickets for himself, and even a couple of my friends. He is so incredibly nice and considerate to me and what I like to do. I will always remember this event and I will always cherish him. On the day of the concert, I got ready, and he picked me up and we went out to a restaurant beforehand. He is so incredibly romantic. He knew exactly where to take me without asking. We ate, laughed, and had a wonderful dinner date before the big event. We arrived at the concert and the music was so incredibly beautiful. I loved every minute of it. My friends, boyfriend, and I all sat down next to each other. As the music was slowly dying down, I found us all getting lost just staring at the stars. It was such an incredibly unforgettable and beautiful night.")[
+                     0])
+
+        del model
+        gc.collect()
+        print(f"sequentiality with seed {i}: {s[-1]}")
+
+    plt.figure()
+    plt.scatter([1 for _ in range(30)], s)
+    plt.show()
+
+    print(s)
 
 
 if __name__ == '__main__':
     # this is how it was run on hpc3 - function is in verification/verify_seq.py
     # verify_data(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
-
-    main()
+    test_model(int(sys.argv[1]), int(sys.argv[2]))
+    # main()
+    # explore_random_seeds()
+    # df = pd.read_csv("./data/hcV3-stories.csv")
+    # create_balanced_dataset(df)
