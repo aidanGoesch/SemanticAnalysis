87b115cc89b8dd04711ddc9bbb6e84de66c0e27c
Aidan Goeschel <aidangoeschel@gmail.com>
2026-07-27 08:15:28 -0700

    added shuffling

--- main.py
+++ main.py
@@ -422,6 +422,7 @@ def event_segmentation(model_id:str):
                         history_lengths=[2, 4, 6, 8], 
                         text_input=[pieman, monkey, tunnel],
                         topics=topics,
+                        n_shuffle=10,
                         save_path="./outputs/ensemble/event_segmentation/")
 
 
